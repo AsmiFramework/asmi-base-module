398650391b0aef741b314ead75d03bcb8c8df0a1
Mahesh J <mahesh.j567@gmail.com>
2016-11-04 11:34:29 +0530

    inheriting from event emitter

--- asmi-base-module.js
+++ asmi-base-module.js
@@ -1,8 +1,13 @@
+var util = require('util');
 var config = require('asmi-config');
+var events = require('events');
 
 function AsmiBaseModule(moduleName) {
+	events.EventEmitter.call(this);
 	this.conf = config.modules[moduleName];
 }
+
+util.inherits(AsmiBaseModule, events.EventEmitter);
 
 var p = AsmiBaseModule.prototype;
 
